1d07b77631dbf1572d85db48d20572d0de2240e8
Giovanni Rivera <grivera64@toromail.csudh.edu>
2021-04-07 21:33:51 -0700

    Create Shopper.java file
    Represent the basic structure of a Shopper in the Supermarket

---
+++ Shopper.java
@@ -0,0 +1,16 @@
+/*
+ * 
+ * Name: Shopper.java
+ * Purpose: Represents the basic structure
+ * 			of a Shopper in the supermarket
+ * Author: grivera64
+ * Date: 04/07/2021
+ * 
+ */
+
+public abstract class Shopper
+{
+	
+	
+	
+}
