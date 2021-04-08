f7c3e9a828b0ca5cad0f6c27bebdcb06f9667bd8
Giovanni Rivera <grivera64@toromail.csudh.edu>
2021-04-08 02:14:20 -0700

    feat(driver): add ModelSupermarket.java
    Adds a driver class for the program

---
+++ ModelSupermarket.java
@@ -0,0 +1,20 @@
+/*
+ * 
+ * Name: ModelSupermarket.java
+ * Purpose: Serves as the driver class of the program
+ * Author: grivera64
+ * Date: 04/08/2021
+ * 
+ */
+
+public class ModelSupermarket
+{
+	
+	public static void main(String[] args)
+	{
+		
+		
+		
+	}
+	
+}
