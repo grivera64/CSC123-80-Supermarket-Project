f4ec9a3820f5d678946f749bacbdf05782ac0287
Giovanni Rivera <grivera64@toromail.csudh.edu>
2021-04-08 00:01:46 -0700

    feat: add BigShopper.java

---
+++ BigShopper.java
@@ -0,0 +1,15 @@
+/*
+ * 
+ * Name: BigShopper.java
+ * Purpose: Represents a shopper in the Supermarket
+ * Author: grivera64
+ * Date: 04/08/2021
+ * 
+*/
+
+public class BigShopper extends Shopper
+{
+	
+	
+	
+}
