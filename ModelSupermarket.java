37cda7932d14ac1980edc99c85e6ca74787693ee
Giovanni Rivera <grivera64@toromail.csudh.edu>
2021-04-08 02:20:49 -0700

    feat(driver): edit ModelSupermarket
    Add functionality for testing classes in driver class

--- ModelSupermarket.java
+++ ModelSupermarket.java
@@ -7,13 +7,36 @@
  * 
  */
 
+import java.util.Scanner;
+import java.io.IOException;
+
 public class ModelSupermarket
 {
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		
+		Scanner keyboard = new Scanner(System.in);
 		
+		System.out.printf("Please enter the name of the Supermarket:\n");
+		String name = keyboard.nextLine();
+		
+		Supermarket market = new Supermarket(name);
+		
+		System.out.printf("Please enter the name of the output file for Supermarket results: ");
+		String fileName = keyboard.nextLine();
+		
+		System.out.printf("Please enter the number of minutes to operate the Supermarket:\n");
+		int minutes = keyboard.nextInt();
+		keyboard.nextLine();
+		
+		market.openSupermarket();
+		
+		market.operateSupermarket(minutes);
+		
+		market.generateSupermarketResults(fileName);
+		
+		keyboard.close();
 		
 	}
 	
