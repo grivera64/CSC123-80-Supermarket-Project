25fb3b866a83faa4067bdc81fb6a39739505042b
Giovanni Rivera <grivera64@toromail.csudh.edu>
2021-04-07 22:51:47 -0700

    feat: add FastShopper.java

---
+++ FastShopper.java
@@ -0,0 +1,15 @@
+/*
+ * 
+ *  Name: FastShopper.java
+ *  Purpose: Represents a Fast Shopper in the supermarket
+ *  Author: grivera64
+ *  Date: 04/07/2021
+ *  
+ */
+
+public class FastShopper extends Shopper
+{
+	
+	
+	
+}
